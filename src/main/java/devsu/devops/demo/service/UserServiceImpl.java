a84cbe81a42ecb3e1a3319a2a887af3696a43393
brandon.diaz <brandon.diaz@blautech.com>
2025-02-09 16:44:28 -0600

    Sample with fixed some issues from sonarqube

--- src/main/java/devsu/devops/demo/service/UserServiceImpl.java
+++ src/main/java/devsu/devops/demo/service/UserServiceImpl.java
@@ -29,7 +29,7 @@ public class UserServiceImpl implements UserService {
 	public List<UserDto> findAll() {
 		List<User> users = userRepository.findAll();
 		return users.stream()
-				.map(user -> userMapper.toDto(user))
+				.map(userMapper::toDto)
 				.collect(Collectors.toList());
 	}
 
